cb7ec0c0bb1b2aebfc734b4dc5989c1b77301738
aria1th <35677394+aria1th@users.noreply.github.com>
2023-12-18 04:48:19 +0900

    Far more optimized

--- extract_exif.py
+++ extract_exif.py
@@ -27,6 +27,7 @@ def read_info_from_image_stealth(image):
     reading_param_len = False
     reading_param = False
     read_end = False
+    never_confirmed = True
     for x in range(width):
         for y in range(height):
             if has_alpha:
@@ -40,10 +41,13 @@ def read_info_from_image_stealth(image):
             buffer_rgb += str(b & 1)
             index_rgb += 3
             if confirming_signature:
+                if x * height + y > 120 and never_confirmed:
+                    return ''
                 if index_a == len('stealth_pnginfo') * 8:
                     decoded_sig = bytearray(int(buffer_a[i:i + 8], 2) for i in
                                             range(0, len(buffer_a), 8)).decode('utf-8', errors='ignore')
                     if decoded_sig in {'stealth_pnginfo', 'stealth_pngcomp'}:
+                        #print(f"Found signature at {x}, {y}")
                         confirming_signature = False
                         sig_confirmed = True
                         reading_param_len = True
@@ -52,6 +56,7 @@ def read_info_from_image_stealth(image):
                             compressed = True
                         buffer_a = ''
                         index_a = 0
+                        never_confirmed = False
                     else:
                         read_end = True
                         break
@@ -59,6 +64,7 @@ def read_info_from_image_stealth(image):
                     decoded_sig = bytearray(int(buffer_rgb[i:i + 8], 2) for i in
                                             range(0, len(buffer_rgb), 8)).decode('utf-8', errors='ignore')
                     if decoded_sig in {'stealth_rgbinfo', 'stealth_rgbcomp'}:
+                        #print(f"Found signature at {x}, {y}")
                         confirming_signature = False
                         sig_confirmed = True
                         reading_param_len = True
@@ -67,6 +73,7 @@ def read_info_from_image_stealth(image):
                             compressed = True
                         buffer_rgb = ''
                         index_rgb = 0
+                        never_confirmed = False
             elif reading_param_len:
                 if mode == 'alpha':
                     if index_a == 32:
@@ -117,6 +124,7 @@ def read_info_from_image_stealth(image):
         except:
             pass
     return str(geninfo)
+
 import tqdm
 import json
 PATH = r"F:\comfyui\ComfyUI\output\NAI_1215"
