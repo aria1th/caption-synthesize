8005af827173c55eaa75285daca1e06a63f71829
AngelBottomless <aria1th@naver.com>
2023-12-21 13:24:25 +0900

    fix bug in prompt

--- query-gemini.py
+++ query-gemini.py
@@ -44,7 +44,8 @@ copyright: touhou
 character: hijiri_byakuren
 general tags: 1girl bangs black_dress blonde_hair blue_eyes blush breasts bridge brown_hair building closed_mouth cloud cross-laced_clothes day dress gradient_hair hair_between_eyes juliet_sleeves layered_dress long_hair long_sleeves looking_at_viewer medium_breasts mountain multicolored_hair outdoors puffy_sleeves purple_hair skirt_hold sky smile solo standing tree turtleneck very_long_hair wavy_hair white_dress
 rating: general
-
+"""
+TEMPLATE_RESULT = r"""
 RESPONSE INCLUDES ALL TAGS GIVEN:
 The character depicted is Hijiri Byakuren from the Touhou series, a 1girl solo standing on a bridge during the day. She has long, very_long_hair with gradient_hair transitioning from purple_hair at the top to blonde_hair at the ends. The sky is visible with a clear day, clouds, and a tree. Buildings, mountain, and outdoors show that the setting is a populated area. Byakuren has blue_eyes, blush on her cheeks, and is looking_at_viewer with a smile and closed_mouth. She is wearing a black_dress paired with a layered_dress and a white_dress beneath. The dress features cross-laced_clothes, turtleneck, long_sleeves, juliet_sleeves, and puffy_sleeves. She has medium_breasts, and is engaging in skirt_hold. Bangs and hair_between_eyes frame her face, and her wavy_hair adds texture to her hairstyle. She seems to have halo on her head, and the illustration is drawn with animation style. The rating is safe.
 
@@ -131,6 +132,7 @@ def generate_text(image_path, return_input=False):
     inputs = [
         TAGS_TEMPLATE,
         image_inference(),
+        TEMPLATE_RESULT,
         tags_formatted('assets/04a0102966be49b7a97548994b228065.jpg'),
         image_inference('assets/04a0102966be49b7a97548994b228065.jpg'),
         "RESPONSE INCLUDES ALL TAGS GIVEN:",
